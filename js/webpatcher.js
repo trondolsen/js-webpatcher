8ca551dceced1ddf487534cddcff327195ef6eb5
Trond Olsen <trond@steinbit.org>
2021-04-14 23:17:15 +0200

    renamed rotary controls

--- js/webpatcher.js
+++ js/webpatcher.js
@@ -4,12 +4,12 @@
  * Licensed under MIT
  */
 
-((browser, ElementSeq, SynthFader, SynthSlider, SynthStepRotary, SynthIntervalRotary, SynthRotary, SynthButton, SynthSwitch, SynthFlipSwitch, SynthPitchBender, SynthKeyboard, Midi) => {
+((browser, ElementSeq, SynthFader, SynthSlider, SynthSteppedRotary, SynthIntervalRotary, SynthFreeRotary, SynthButton, SynthSwitch, SynthFlipSwitch, SynthPitchBender, SynthKeyboard, Midi) => {
   'use strict';
 
   const config = {
     title: 'Applications',
-    color: 'blue',
+    color: 'classic',
     mouse: {
       sensitivity: 100.0
     },
@@ -33,7 +33,7 @@
 
     // Create Synth controls
     var synth = {
-      lfo1Waveform: new SynthStepRotary(query('.synth-lfo1-waveform')),
+      lfo1Waveform: new SynthSteppedRotary(query('.synth-lfo1-waveform')),
       lfo1Rate: new SynthFader(query('.synth-lfo1-rate')),
       lfo1Delay: new SynthFader(query('.synth-lfo1-delay')),
       lfo1Lrphase: new SynthFader(query('.synth-lfo1-lrphase')),
@@ -46,10 +46,10 @@
       ddsmodPwm: new SynthFader(query('.synth-ddsmod-pwm')),
       ddsmodPwmsource: new SynthSwitch(query('.synth-ddsmod-pwmsource')),
       ddsmodCrossmod: new SynthFader(query('.synth-ddsmod-crossmod')),
-      dds1Waveform: new SynthStepRotary(query('.synth-dds1-waveform')),
-      dds1Range: new SynthStepRotary(query('.synth-dds1-range')),
-      dds2Waveform: new SynthStepRotary(query('.synth-dds2-waveform')),
-      dds2Range: new SynthStepRotary(query('.synth-dds2-range')),
+      dds1Waveform: new SynthSteppedRotary(query('.synth-dds1-waveform')),
+      dds1Range: new SynthSteppedRotary(query('.synth-dds1-range')),
+      dds2Waveform: new SynthSteppedRotary(query('.synth-dds2-waveform')),
+      dds2Range: new SynthSteppedRotary(query('.synth-dds2-range')),
       dds2Tune: new SynthIntervalRotary(query('.synth-dds2-tune')),
       dds2Mode: new SynthSwitch(query('.synth-dds2-mode')),
       mixerMix: new SynthIntervalRotary(query('.synth-mixer-mix')),
@@ -99,7 +99,7 @@
       seqOn: new SynthButton(query('.synth-seq-on')),
       seqHold: new SynthButton(query('.synth-seq-hold')),
       seqSeqrec: new SynthButton(query('.synth-seq-seqrec')),
-      modAmount: new SynthRotary(query('.synth-mod-amount')),
+      modAmount: new SynthFreeRotary(query('.synth-mod-amount')),
       modModassign: new SynthButton(query('.synth-mod-modassign')),
       mod1: new SynthButton(query('.synth-mod-1')),
       mod2: new SynthButton(query('.synth-mod-2')),
@@ -957,7 +957,7 @@
    *  Synth StepRotary
    */
 
-  class SynthStepRotary {
+  class SynthSteppedRotary {
     constructor(elem, stepValues = [-75, -45, -15, 15, 45, 75], sensitivity = 0.5) {
       if (elem === null || stepValues === null || sensitivity === null) {
         throw Error('Constructor parameters was null');
@@ -995,7 +995,7 @@
     }
   }
 
-  return SynthStepRotary;
+  return SynthSteppedRotary;
 }(window))),(window, (function(browser) {
   'use strict';
 
@@ -1050,10 +1050,10 @@
   'use strict';
 
   /*
-   *  Synth Rotary
+   *  Synth Free Rotary
    */
 
-  class SynthRotary {
+  class SynthFreeRotary {
     constructor(elem, sensitivity = 0.1) {
       if (elem === null || sensitivity === null) {
         throw Error('Constructor parameters was null');
@@ -1082,7 +1082,7 @@
     }
   }
 
-  return SynthRotary;
+  return SynthFreeRotary;
 }(window))),(window, (function(browser) {
   'use strict';
 
